d390bcb085696e9b1204f640fec1e68a0a7af605
Remigiusz Marciniak <remigiuszmarciniakpl@gmail.com>
2018-05-15 22:19:17 +0200

    15.V.18

--- app/src/main/java/marciniak/remigiusz/strzykawkagertruda/MainActivity.java
+++ app/src/main/java/marciniak/remigiusz/strzykawkagertruda/MainActivity.java
@@ -2,6 +2,8 @@ package marciniak.remigiusz.strzykawkagertruda;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,30 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button buttonLogin = (Button)findViewById(R.id.buttonLogin);
+        buttonLogin.setOnClickListener(new View.OnClickListener()
+        {
+
+            @Override
+            public void onClick(View v) {
+            }
+        });
+
+
+    }
+
+    public void buttonSettingsClickHandler(View view) {
+
+    }
+
+
+    class buttonLoginClass implements View.OnClickListener
+    {
+
+        @Override
+        public void onClick(View v) {
+
+        }
     }
 }
