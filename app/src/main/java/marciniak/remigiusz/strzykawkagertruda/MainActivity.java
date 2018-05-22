dcbe74d17ceef81f2ed4e74b3ab97f6b863d2d79
Remigiusz Marciniak <remigiuszmarciniakpl@gmail.com>
2018-05-22 13:58:12 +0200

    22.V.18

--- app/src/main/java/marciniak/remigiusz/strzykawkagertruda/MainActivity.java
+++ app/src/main/java/marciniak/remigiusz/strzykawkagertruda/MainActivity.java
@@ -1,40 +1,57 @@
 package marciniak.remigiusz.strzykawkagertruda;
 
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    public void clickFunction(View view)
+    {
+        Log.i("info", "button clicked");
+    }
+    public void logInClicked(View view){
+
+        EditText putEmailEditText = (EditText) findViewById(R.id.putEmailEditText);
+        EditText putPasswordEditText = (EditText) findViewById(R.id.putPasswordEditText);
+
+        Log.i("Username", putEmailEditText.getText().toString());
+        Log.i("password", putPasswordEditText.getText().toString());
+
+        Toast.makeText(MainActivity.this,"Zalogowano!",Toast.LENGTH_SHORT).show();
+    }
+
+    public void buttonInfo(View view){
+        Log.i("info","button info");
+
+        ImageView  imageViewStrzykawkaGertruda = (ImageView) findViewById(R.id.imageViewStrzykawkaGertruda);
+        imageViewStrzykawkaGertruda.setImageResource(R.drawable.strzykawkagertrudasloneczko);
+
+        /// jak zrobic zeby byla zmiana pomiedzy dwoma zdjeciami, (while?)
+        if( imageViewStrzykawkaGertruda == (ImageView) findViewById(R.id.imageViewStrzykawkaGertruda))
+        {
+            imageViewStrzykawkaGertruda.setImageResource(R.drawable.strzykawkagertrudasloneczko);
+
+        }
+        else
+        {
+            imageViewStrzykawkaGertruda.setImageResource(R.drawable.strzykawkagertruda);
+        }
+    }
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Button buttonLogin = (Button)findViewById(R.id.buttonLogin);
-        buttonLogin.setOnClickListener(new View.OnClickListener()
-        {
-
-            @Override
-            public void onClick(View v) {
-            }
-        });
-
-
-    }
-
-    public void buttonSettingsClickHandler(View view) {
-
     }
 
 
-    class buttonLoginClass implements View.OnClickListener
-    {
-
-        @Override
-        public void onClick(View v) {
-
-        }
-    }
 }
