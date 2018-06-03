8a5a39c4f4bb4688998db890c24f3d5a5a5ea39a
Remigiusz Marciniak <remigiuszmarciniakpl@gmail.com>
2018-06-04 00:32:09 +0200

    4.VI.18

--- app/src/main/java/marciniak/remigiusz/strzykawkagertruda/MainActivity.java
+++ app/src/main/java/marciniak/remigiusz/strzykawkagertruda/MainActivity.java
@@ -14,35 +14,83 @@ public class MainActivity extends AppCompatActivity {
 
     public void clickFunction(View view)
     {
-        Log.i("info", "button clicked");
+        Log.i("info", "pomocy");
+        ImageView imageViewStrzykawkaGertruda = (ImageView) findViewById(R.id.imageViewStrzykawkaGertruda);
+        ImageView imageViewStrzykawkaGertrudaSloneczko = (ImageView) findViewById(R.id.imageViewStrzykawkaGertrudaSloneczko);
+
+        imageViewStrzykawkaGertruda.animate().scaleX(0.75f).scaleY(0.75f).rotation(3600f).setDuration(1000);
+        imageViewStrzykawkaGertrudaSloneczko.animate().scaleX(0.75f).scaleY(0.75f).rotation(3600f).setDuration(1000);
+
+        //zmiana kata z jeden na drugi plynna
+//        if(imageViewStrzykawkaGertruda.getRotation() == 180 || imageViewStrzykawkaGertrudaSloneczko.getRotation() == 180)
+//        {
+//            imageViewStrzykawkaGertruda.animate().rotation(-180f).setDuration(1000);
+//            imageViewStrzykawkaGertrudaSloneczko.animate().rotation(-180f).setDuration(1000);
+//            Log.i("info", "pomocy1");
+//        }
+//        else{
+//            imageViewStrzykawkaGertruda.animate().rotation(3600f).setDuration(1000);
+//            imageViewStrzykawkaGertrudaSloneczko.animate().rotation(3600f).setDuration(1000);
+//            Log.i("info", "pomocy2");
+//        }
+//
+//        imageViewStrzykawkaGertruda.animate().rotation(180f).setDuration(1000);
+//        imageViewStrzykawkaGertrudaSloneczko.animate().rotation(180f).setDuration(1000);
+
+
+        //czemu nie ma tak plynego przejscia
+//        for(int i = 0; i<10; i++){
+//            Log.i("info", "petla start" + i);
+//            imageViewStrzykawkaGertruda.animate().alpha(0f).setDuration(1000*i);
+//            imageViewStrzykawkaGertrudaSloneczko.animate().alpha(1f).setDuration(3000*i);
+//            imageViewStrzykawkaGertruda.animate().alpha(1f).setDuration(4000*i);
+//            imageViewStrzykawkaGertrudaSloneczko.animate().alpha(0f).setDuration(7000*i);
+//            Log.i("info", "petla koniec" + i);
+//        }
+
     }
     public void logInClicked(View view){
 
         EditText putEmailEditText = (EditText) findViewById(R.id.putEmailEditText);
         EditText putPasswordEditText = (EditText) findViewById(R.id.putPasswordEditText);
 
+        /// if else zeby przy zalogowaniu pokazywalo sie zalogowano
         Log.i("Username", putEmailEditText.getText().toString());
-        Log.i("password", putPasswordEditText.getText().toString());
+     //   Log.i("password", putPasswordEditText.getText().toString());
 
         Toast.makeText(MainActivity.this,"Zalogowano!",Toast.LENGTH_SHORT).show();
     }
 
+    public void fade(View view) {
+       Log.i("Strzykawka clicked", "strzykawka kliknieta" );
+       ImageView imageViewStrzykawkaGertruda = (ImageView) findViewById(R.id.imageViewStrzykawkaGertruda);
+       ImageView imageViewStrzykawkaGertrudaSloneczko = (ImageView) findViewById(R.id.imageViewStrzykawkaGertrudaSloneczko);
+       imageViewStrzykawkaGertruda.animate().alpha(0f).setDuration(1000);
+       imageViewStrzykawkaGertrudaSloneczko.animate().alpha(1f).setDuration(3000);
+
+
+
+    }
+
+//    public void fadeSloneczko(View view) {
+//        Log.i("Strzykawka clicked", "strzykawka kliknieta" );
+//        ImageView imageViewStrzykawkaGertruda = (ImageView) findViewById(R.id.imageViewStrzykawkaGertruda);
+//        ImageView imageViewStrzykawkaGertrudaSloneczko = (ImageView) findViewById(R.id.imageViewStrzykawkaGertrudaSloneczko);
+//
+//        imageViewStrzykawkaGertrudaSloneczko.animate().alpha(0f).setDuration(1000);
+//        imageViewStrzykawkaGertruda.animate().alpha(1f).setDuration(3000);
+//
+//    }
+
     public void buttonInfo(View view){
         Log.i("info","button info");
 
-        ImageView  imageViewStrzykawkaGertruda = (ImageView) findViewById(R.id.imageViewStrzykawkaGertruda);
-        imageViewStrzykawkaGertruda.setImageResource(R.drawable.strzykawkagertrudasloneczko);
+        ImageView imageViewStrzykawkaGertruda = (ImageView) findViewById(R.id.imageViewStrzykawkaGertruda);
+        ImageView imageViewStrzykawkaGertrudaSloneczko = (ImageView) findViewById(R.id.imageViewStrzykawkaGertrudaSloneczko);
+        imageViewStrzykawkaGertrudaSloneczko.animate().alpha(0f).setDuration(1000);
+        imageViewStrzykawkaGertruda.animate().alpha(1f).setDuration(3000);
 
         /// jak zrobic zeby byla zmiana pomiedzy dwoma zdjeciami, (while?)
-        if( imageViewStrzykawkaGertruda == (ImageView) findViewById(R.id.imageViewStrzykawkaGertruda))
-        {
-            imageViewStrzykawkaGertruda.setImageResource(R.drawable.strzykawkagertrudasloneczko);
-
-        }
-        else
-        {
-            imageViewStrzykawkaGertruda.setImageResource(R.drawable.strzykawkagertruda);
-        }
     }
 
 
@@ -50,6 +98,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ImageView imageViewStrzykawkaGertrudaSloneczko = (ImageView) findViewById(R.id.imageViewStrzykawkaGertrudaSloneczko);
+
 
     }
 
