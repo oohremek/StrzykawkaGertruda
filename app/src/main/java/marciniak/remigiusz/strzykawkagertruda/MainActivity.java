984f9228804a19f516c517b1318c80b6db6766e9
Remigiusz Marciniak <remigiuszmarciniakpl@gmail.com>
2018-06-05 00:17:02 +0200

    5.VI.18

--- app/src/main/java/marciniak/remigiusz/strzykawkagertruda/MainActivity.java
+++ app/src/main/java/marciniak/remigiusz/strzykawkagertruda/MainActivity.java
@@ -1,5 +1,6 @@
 package marciniak.remigiusz.strzykawkagertruda;
 
+import android.content.Intent;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -9,33 +10,51 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.Toast;
+import android.media.MediaPlayer;
 
 public class MainActivity extends AppCompatActivity {
+
+    //zasieg zmiennych globalny
+    private MediaPlayer mPlayerPomoc;
+    private MediaPlayer mPlayerCoTo;
+    private ImageView imageViewStrzykawkaGertruda;
+    private ImageView imageViewStrzykawkaGertrudaSloneczko;
+    private EditText putEmailEditText;
+    private EditText putPasswordEditText;
+    private MediaPlayer mPlayerWitam;
+    private Button buttonSettings;
+
+
+
+    public void buttonInfo(View view){
+        Log.i("info","button info");
+
+        mPlayerCoTo.start();
+
+
+
+
+
+        imageViewStrzykawkaGertrudaSloneczko.animate().alpha(0f).setDuration(1000);
+        imageViewStrzykawkaGertruda.animate().alpha(1f).setDuration(3000);
+
+
+        /// jak zrobic zeby byla zmiana pomiedzy dwoma zdjeciami, (while?)
+    }
+
 
     public void clickFunction(View view)
     {
         Log.i("info", "pomocy");
-        ImageView imageViewStrzykawkaGertruda = (ImageView) findViewById(R.id.imageViewStrzykawkaGertruda);
-        ImageView imageViewStrzykawkaGertrudaSloneczko = (ImageView) findViewById(R.id.imageViewStrzykawkaGertrudaSloneczko);
+
+        mPlayerPomoc.start();
+
+
 
         imageViewStrzykawkaGertruda.animate().scaleX(0.75f).scaleY(0.75f).rotation(3600f).setDuration(1000);
         imageViewStrzykawkaGertrudaSloneczko.animate().scaleX(0.75f).scaleY(0.75f).rotation(3600f).setDuration(1000);
 
         //zmiana kata z jeden na drugi plynna
-//        if(imageViewStrzykawkaGertruda.getRotation() == 180 || imageViewStrzykawkaGertrudaSloneczko.getRotation() == 180)
-//        {
-//            imageViewStrzykawkaGertruda.animate().rotation(-180f).setDuration(1000);
-//            imageViewStrzykawkaGertrudaSloneczko.animate().rotation(-180f).setDuration(1000);
-//            Log.i("info", "pomocy1");
-//        }
-//        else{
-//            imageViewStrzykawkaGertruda.animate().rotation(3600f).setDuration(1000);
-//            imageViewStrzykawkaGertrudaSloneczko.animate().rotation(3600f).setDuration(1000);
-//            Log.i("info", "pomocy2");
-//        }
-//
-//        imageViewStrzykawkaGertruda.animate().rotation(180f).setDuration(1000);
-//        imageViewStrzykawkaGertrudaSloneczko.animate().rotation(180f).setDuration(1000);
 
 
         //czemu nie ma tak plynego przejscia
@@ -51,20 +70,20 @@ public class MainActivity extends AppCompatActivity {
     }
     public void logInClicked(View view){
 
-        EditText putEmailEditText = (EditText) findViewById(R.id.putEmailEditText);
-        EditText putPasswordEditText = (EditText) findViewById(R.id.putPasswordEditText);
+
 
         /// if else zeby przy zalogowaniu pokazywalo sie zalogowano
         Log.i("Username", putEmailEditText.getText().toString());
      //   Log.i("password", putPasswordEditText.getText().toString());
 
         Toast.makeText(MainActivity.this,"Zalogowano!",Toast.LENGTH_SHORT).show();
+
+
     }
 
     public void fade(View view) {
        Log.i("Strzykawka clicked", "strzykawka kliknieta" );
-       ImageView imageViewStrzykawkaGertruda = (ImageView) findViewById(R.id.imageViewStrzykawkaGertruda);
-       ImageView imageViewStrzykawkaGertrudaSloneczko = (ImageView) findViewById(R.id.imageViewStrzykawkaGertrudaSloneczko);
+
        imageViewStrzykawkaGertruda.animate().alpha(0f).setDuration(1000);
        imageViewStrzykawkaGertrudaSloneczko.animate().alpha(1f).setDuration(3000);
 
@@ -74,34 +93,50 @@ public class MainActivity extends AppCompatActivity {
 
 //    public void fadeSloneczko(View view) {
 //        Log.i("Strzykawka clicked", "strzykawka kliknieta" );
-//        ImageView imageViewStrzykawkaGertruda = (ImageView) findViewById(R.id.imageViewStrzykawkaGertruda);
-//        ImageView imageViewStrzykawkaGertrudaSloneczko = (ImageView) findViewById(R.id.imageViewStrzykawkaGertrudaSloneczko);
+
 //
 //        imageViewStrzykawkaGertrudaSloneczko.animate().alpha(0f).setDuration(1000);
 //        imageViewStrzykawkaGertruda.animate().alpha(1f).setDuration(3000);
 //
 //    }
 
-    public void buttonInfo(View view){
-        Log.i("info","button info");
-
-        ImageView imageViewStrzykawkaGertruda = (ImageView) findViewById(R.id.imageViewStrzykawkaGertruda);
-        ImageView imageViewStrzykawkaGertrudaSloneczko = (ImageView) findViewById(R.id.imageViewStrzykawkaGertrudaSloneczko);
-        imageViewStrzykawkaGertrudaSloneczko.animate().alpha(0f).setDuration(1000);
-        imageViewStrzykawkaGertruda.animate().alpha(1f).setDuration(3000);
-
-        /// jak zrobic zeby byla zmiana pomiedzy dwoma zdjeciami, (while?)
-    }
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        ImageView imageViewStrzykawkaGertrudaSloneczko = (ImageView) findViewById(R.id.imageViewStrzykawkaGertrudaSloneczko);
 
+        //inicjalizacja zmiennych
+        mPlayerCoTo = MediaPlayer.create(this, R.raw.strzykawkacotojest);
+        mPlayerPomoc = MediaPlayer.create(this, R.raw.strzykawkapomoc);
 
+        imageViewStrzykawkaGertruda = (ImageView) findViewById(R.id.imageViewStrzykawkaGertruda);
+        imageViewStrzykawkaGertrudaSloneczko = (ImageView) findViewById(R.id.imageViewStrzykawkaGertrudaSloneczko);
+
+        putEmailEditText = (EditText) findViewById(R.id.putEmailEditText);
+        putPasswordEditText = (EditText) findViewById(R.id.putPasswordEditText);
+
+        mPlayerWitam = MediaPlayer.create(this, R.raw.strzykawkagertruda);
+
+        //akcja
+        mPlayerWitam.start();
+        //
+
+        buttonSettings = (Button) findViewById(R.id.buttonSettings);
+
+        buttonSettings.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openActivitySettings();
+            }
+        });
     }
 
+    public void openActivitySettings(){
+
+        Intent intent = new Intent(this,Settings.class);
+        startActivity(intent);
+    }
 
 }
--- app/src/main/java/marciniak/remigiusz/strzykawkagertruda/Settings.java
+++ app/src/main/java/marciniak/remigiusz/strzykawkagertruda/Settings.java
@@ -1,13 +1,33 @@
 package marciniak.remigiusz.strzykawkagertruda;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Settings extends AppCompatActivity {
+
+    private Button buttonBack;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
+
+
+        buttonBack = (Button) findViewById(R.id.buttonBack);
+        buttonBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openMainActivity();
+            }
+        });
+    }
+
+    public void openMainActivity(){
+        Intent intent = new Intent(this,MainActivity.class);
+        startActivity(intent);
     }
 }
